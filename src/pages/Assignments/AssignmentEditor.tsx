b96f202f7f77c60c83b7cb16b0a020ee7ae05b2b
smule <smule@ncsu.edu>
2023-12-05 03:00:36 -0500

    createAssignment

--- src/pages/Assignments/AssignmentEditor.tsx
+++ src/pages/Assignments/AssignmentEditor.tsx
@@ -2,83 +2,80 @@ import * as Yup from "yup";
 
 import { Button, Modal } from "react-bootstrap";
 import { Form, Formik, FormikHelpers } from "formik";
-import { IAssignmentRequest, transformAssignmentRequest } from "./AssignmentUtil";
-import { IEditor, ROLE } from "../../utils/interfaces";
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
-import FormInput from "components/Form/FormInput";
+import FormInput from "components/Form/FormInput"; // Import your custom FormInput component
 import { HttpMethod } from "utils/httpMethods";
 import { RootState } from "../../store/store";
 import { alertActions } from "store/slices/alertSlice";
+import { transformAssignmentRequest } from "./AssignmentUtil";
 import useAPI from "hooks/useAPI";
+import { useDispatch } from "react-redux";
 
-const initialValues: IAssignmentRequest = {
+interface AssignmentEditorProps {
+  mode: string;
+}
+
+const initialValues = {
   name: "",
-  description: "",
-  dueDate: "",
-  // Add other assignment-specific initial values
+  created_at: "",
+  updated_at: "",
 };
 
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
-  description: Yup.string().required("Required"),
-  // Add other assignment-specific validation rules
+  created_at: Yup.string().required("Required"),
+  updated_at: Yup.string().required("Required"),
 });
 
-const AssignmentEditor: React.FC<IEditor> = ({ mode }) => {
+const AssignmentEditor: React.FC<AssignmentEditorProps> = ({ mode }) => {
   const { data: assignmentResponse, error: assignmentError, sendRequest } = useAPI();
-  const auth = useSelector((state: RootState) => state.authentication);
-  const { assignmentData }: any = useLoaderData(); // Adjust as needed
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
 
-  // Close the modal if the assignment is updated successfully and navigate to the assignments page
   useEffect(() => {
-    if (
-      assignmentResponse &&
-      assignmentResponse.status &&
-      assignmentResponse.status >= 200 &&
-      assignmentResponse.status < 300 &&
-      assignmentData
-    ) {
+    if (assignmentResponse && assignmentResponse.status >= 200 && assignmentResponse.status < 300) {
       dispatch(
         alertActions.showAlert({
           variant: "success",
-          message: `Assignment ${assignmentData.name} ${mode}d successfully!`,
+          message: `Assignment ${initialValues.name} ${mode === "update" ? "updated" : "created"} successfully!`,
         })
       );
       navigate(location.state?.from ? location.state.from : "/assignments");
     }
-  }, [dispatch, mode, navigate, assignmentData, assignmentResponse, location.state?.from]);
-
-  // Show the error message if the assignment is not updated successfully
-  useEffect(() => {
-    assignmentError && dispatch(alertActions.showAlert({ variant: "danger", message: assignmentError }));
-  }, [assignmentError, dispatch]);
+  
+    // Handle assignment creation/update error
+    if (assignmentError) {
+      dispatch(alertActions.showAlert({ variant: "danger", message: assignmentError }));
+    }
+  }, [dispatch, assignmentResponse, assignmentError, navigate, location]);
 
   const onSubmit = (
-    values: IAssignmentRequest,
-    submitProps: FormikHelpers<IAssignmentRequest>
+    values: { name: string; created_at: string; updated_at: string },
+    submitProps: FormikHelpers<{ name: string; created_at: string; updated_at: string }>
   ) => {
-    let method: HttpMethod = HttpMethod.POST;
-    let url: string = "/assignments";
+    let method: HttpMethod = HttpMethod.POST; // Change as needed
+    let url: string = "/assignments"; // Change as needed
 
     if (mode === "update") {
-      url = `/assignments/${values.id}`;
+      url = `/assignments/${values.name}`;
       method = HttpMethod.PATCH;
     }
+    // Customize the request data based on your API requirements
+    const requestData = {
+      // ...
+    };
 
-    // to be used to display message when assignment is created
-    assignmentData.name = values.name;
     sendRequest({
       url: url,
       method: method,
       data: values,
-      transformRequest: transformAssignmentRequest,
+      transformRequest: transformAssignmentRequest
+      // Add any other necessary options
     });
+
     submitProps.setSubmitting(false);
   };
 
@@ -87,40 +84,37 @@ const AssignmentEditor: React.FC<IEditor> = ({ mode }) => {
   return (
     <Modal size="lg" centered show={true} onHide={handleClose} backdrop="static">
       <Modal.Header closeButton>
-        <Modal.Title>{mode === "update" ? "Update Assignment" : "Create Assignment"}</Modal.Title>
+        <Modal.Title>Create/Update Assignment</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {assignmentError && <p className="text-danger">{assignmentError}</p>}
         <Formik
-          initialValues={mode === "update" ? assignmentData : initialValues}
+          initialValues={initialValues}
           onSubmit={onSubmit}
           validationSchema={validationSchema}
           validateOnChange={false}
           enableReinitialize={true}
         >
-          {(formik) => {
-            return (
-              <Form>
-                <FormInput controlId="assignment-name" label="Name" name="name" />
-                <FormInput controlId="assignment-description" label="Description" name="description" />
-                {/* Add other assignment-specific form fields here */}
+          {(formik) => (
+            <Form>
+              <FormInput controlId="assignment-name" label="Name" name="name" />
+              <FormInput controlId="assignment-created-at" label="Created At" name="created_at" />
+              <FormInput controlId="assignment-updated-at" label="Updated At" name="updated_at" />
 
-                <Modal.Footer>
-                  <Button variant="outline-secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-
-                  <Button
-                    variant="outline-success"
-                    type="submit"
-                    disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
-                  >
-                    {mode === "update" ? "Update Assignment" : "Create Assignment"}
-                  </Button>
-                </Modal.Footer>
-              </Form>
-            );
-          }}
+              <Modal.Footer>
+                <Button variant="outline-secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button
+                  variant="outline-success"
+                  type="submit"
+                  disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
+                >
+                  Create/Update Assignment
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
         </Formik>
       </Modal.Body>
     </Modal>
